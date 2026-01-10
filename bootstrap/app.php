3d6c259fd8295d404e13c223110de972d55dd416
S. Maulana Muhammad Rafid <rafid@Maulanas-Brick.local>
2026-01-10 23:29:54 +0700

    fix cors part 3

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -20,6 +20,7 @@ return Application::configure(basePath: dirname(__DIR__))
             'verified' => \App\Http\Middleware\EnsureEmailIsVerified::class,
         ]);
 
+        $middleware->trustProxies(at: '*');
         //
     })
     ->withExceptions(function (Exceptions $exceptions): void {
